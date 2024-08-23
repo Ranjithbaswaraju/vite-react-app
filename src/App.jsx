243c630e0c752ec5318ff0c5e732d177678afa4c
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2024-08-23 11:53:26 +0530

    bootstrap-webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,17 @@
-
-import Button from "./Button/button"
-import Heading from "./header/header"
-import CustomRectangleCard from './components/bootstrap/custom-card'
-import { employeeDetails } from "./data/employedata"
-import FirstComponent from "./components/bootstrap/first-component"
+import Header from "./components/bootstrap/bootstrapheader"
+import Footer from "./components/bootstrap/footer";
+import Card from "./components/bootstrap/first-component"
 
 
 const App=()=>{
-
-
-  // return(
-  //  <div>
-  //   {/* <Button/>
-  //   <Heading/> */}
-  
-  //   <CustomRectangleCard text="Ranjith" title="React developer"/>
-  //  </div>
-   
-    
-  // )
-
   return(
-    <div>
-<FirstComponent/>
-
-      {/* {
-        employeeDetails.map(each=>{
-          return(
-            <>
-            <CustomRectangleCard text={each.name} title={each.designation}/>
-            </>
-          )
-        })
-      } */}
-    </div>
+    <>
+    
+    <Header/>
+    <Card/>
+    <Footer/>
+    </>
+    
   )
 }
 export default App;
--- src/components/bootstrap/custom-card.jsx
+++ src/components/bootstrap/custom-card.jsx
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card';
 
 function CustomRectangleCard({text,title}) {
   return (
-    <Card style={{ width: '10rem' }}>
+    <Card style={{ width: '18rem' }}>
       {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
       <Card.Body>
         <Card.Title>{text}</Card.Title>
