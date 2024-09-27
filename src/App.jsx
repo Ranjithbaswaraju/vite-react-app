2ec8975213cc15d6613a84ecb6abb4ab75c99f3b
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2024-09-27 11:28:22 +0530

    google

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 // import UseEffectExample from "./components/hooks/useEffect/useEffcetEx";
 // import UseEffectExample2 from "./components/hooks/useEffect/useEffectEx2";
+// import { GoogleApiWrapper } from "google-maps-react";
 import UseEffectExample3 from "./components/hooks/useEffect/useEffectEx3";
 import TableComponet from "./mock/table";
+import { MapContainer } from "./google/google";
 
 
 
@@ -16,8 +18,8 @@ const App=()=>{
 {/* <UseEffectExample/> */}
 {/* <UseEffectExample2/> */}
 {/* <UseEffectExample3/> */}
-<TableComponet/>
-
+{/* <TableComponet/> */}
+<MapContainer/>
     </>
   )
 }
