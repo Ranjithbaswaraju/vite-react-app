05859d393d4b03c35b29c69f41ca6f18db696197
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-01-03 17:18:58 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 import Even from "./components/hooks/thtme/evenorodd";
+import EvenOddBackground from "./components/hooks/thtme/background";
 
 
 
 const App = () => {
   return (
     <>
-      <Even/>
+      {/* <Even/> */}
+      <EvenOddBackground/>
     </>
   );
 }
