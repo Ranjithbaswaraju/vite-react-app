da15d739a9c8f2179399b831299cff2acc3ad411
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-01-08 17:38:51 +0530

    example

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Ranjith from "./components/hooks/thtme/themeChngaer";
+import UseStateCounter from "./Practice/useState";
 
-import Counter from "./Practice/classcounter";
-import Youtube from "./Practice/youtube";
+
 
 const App = () => {
   return (
     <>
-     {/* <Counter/> */}
-     <Youtube/>
+    <UseStateCounter/>
     </>
   );
 }
---
+++ src/Practice/useState.jsx
@@ -0,0 +1,37 @@
+
+import React from "react";
+import { useState } from "react";
+
+const UseStateCounter=()=>{
+    const[count,setCount]=useState(0)
+
+    const incrementHandler=()=>{
+        setCount(count+1)
+    }
+    const decrementHandler=()=>{
+        if(count>=1){
+            setCount(count-1)
+        }
+        else{
+            alert("Count is not in negative")
+        }
+    }
+    const resetHandler=()=>[
+        setCount(0)
+    ]
+
+    return(
+
+        
+        <>
+        <h2>Count:{count}</h2>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={resetHandler}>Rest</button>
+        
+        
+        </>
+    )
+
+}
+export default UseStateCounter
