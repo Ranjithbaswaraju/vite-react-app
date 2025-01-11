3aaf58a2d8a8cee0981ed81992db034b4b915ed0
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-01-11 23:09:55 +0530

    updated

--- src/components/hooks/thtme/input.jsx
+++ src/components/hooks/thtme/input.jsx
@@ -1,21 +1,47 @@
-import React from "react";
-import { useState } from "react";
-const EvenOddChecker=()=>{
-  const[number,setNumber]=useState("")
-  const changeHandler=(event)=>{
-    const finalNumber=event.target.value
+// import React from "react";
+// import { useState } from "react";
+// const EvenOddChecker=()=>{
+//   const[number,setNumber]=useState("")
+//   const changeHandler=(event)=>{
+//     const finalNumber=event.target.value
   
-    if(isNaN(finalNumber) || finalNumber=== ""){
+//     if(isNaN(finalNumber) || finalNumber=== ""){
+//       setNumber("NaN")
+//     }
+//     else{
+//       setNumber(finalNumber %2 === 0 ? `${finalNumber} is Even`:`${finalNumber} is Odd`)
+//     } 
+//   }
+//   return(
+//     <>
+//     <input type="text" placeholder="Enter a Number" onChange={changeHandler}/>
+//     <input type="text" placeholder="Number is " value={number}/>
+//     </>
+//   )
+// }
+// export default EvenOddChecker
+
+import React, { useState } from "react";
+
+const EvenOddChecker=()=>{
+
+  const[number,setNumber]=useState('')
+
+  const evenHandler=(event)=>{
+    const finalNumber=(event.target.value)
+
+    if(isNaN(finalNumber) || finalNumber==""){
       setNumber("NaN")
     }
     else{
-      setNumber(finalNumber %2 === 0 ? `${finalNumber} is Even`:`${finalNumber} is Odd`)
-    } 
+      setNumber(finalNumber%2==0?`${finalNumber} is Even `:`${finalNumber} is Odd`)
+    }
   }
+
   return(
     <>
-    <input type="text" placeholder="Enter a Number" onChange={changeHandler}/>
-    <input type="text" placeholder="Number is " value={number}/>
+    <input type="text" onChange={evenHandler} placeholder="Enter a Number"/>
+    <input type="text" value={number}/>
     </>
   )
 }
