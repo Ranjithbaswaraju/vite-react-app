f4c73e20ce02fdc421aee84d781c6243de54d706
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-01-11 22:12:45 +0530

    updated

--- src/components/hooks/watch/stopwatch.jsx
+++ src/components/hooks/watch/stopwatch.jsx
@@ -1,20 +1,24 @@
-import React, { useState,useEffect } from "react";
+
+
+
+import React, { useEffect, useState } from "react";
 const Stopwatch=()=>{
 
-    const[time,setTime]=useState('')
-    const[count,setCount]=useState(true)
+    const[time,setTime]=useState(0)
+    const[count,setCount]=useState(false)
 
-    useEffect(() => {
-            let intervalId;
-             if (count) {
-               intervalId = setInterval(() => {
-                setTime((time) => time + 1);
-               }, 1000);
-             }
-             return () => {
-               clearInterval(intervalId);
-             };
-           }, [count]);
+    useEffect(()=>{
+        let intervalId;
+        if(count){
+            intervalId=setInterval(()=>{
+                setTime((time)=>time+1)
+            },1000)
+        }
+        return ()=>{
+            clearInterval(intervalId)
+        }
+    })
+
     const startHandler=()=>{
         setCount(true)
     }
@@ -25,23 +29,21 @@ const Stopwatch=()=>{
         setTime(0)
         setCount(false)
     }
-    const hours = Math.floor(time / 3600).toString().padStart(2, '0');
-    const minutes = Math.floor((time % 3600) / 60).toString().padStart(2, '0');
-    const seconds = (time % 60).toString().padStart(2, '0');
+
+    const hours=Math.floor(time/3600).toString().padStart(2,'0')
+    const minutes=Math.floor((time%3600)/60).toString().padStart(2,'0')
+    const seconds=(time%60).toString().padStart(2,'0')
     return(
-        <>
-        <div style={{height:'100vh',width:'100vw',display:'flex',justifyContent:'center',alignItems:'center'}}>
-        <div style={{height:'150px',width:'300px',backgroundColor:'aqua' ,textAlign:'center',}}>
-            <h2 style={{textAlign:'center'}}>STOP WATCH</h2>
-            
-        <h2>{hours}:{minutes}:{seconds}</h2>
-        <button onClick={startHandler}>Start</button>
-        <button onClick={stopHandler}>Stop</button>
-        <button onClick={resetHandler}>Reset</button>
+        <div style={{height:"100vh",width:'100vw',display:'flex',justifyContent:'center',alignItems:'center'}}>
+            <div style={{height:'150px',width:'300px',textAlign:"center",backgroundColor:'aqua'}}>
+                    <h3>STOP WATCH</h3>
+
+            <h5>{hours}:{minutes}:{seconds}</h5>
+            <button onClick={startHandler}>START</button>
+            <button onClick={stopHandler}>STOP</button>
+            <button onClick={resetHandler}>RESET</button>
+            </div>
         </div>
-        </div>
-        
-        </>
     )
 }
-export default Stopwatch 
+export default Stopwatch
