05650745bca1591fb3b3d781e834549d7f4bee22
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-01-12 22:28:53 +0530

    updated

--- src/Practice/useReduceradding.jsx
+++ src/Practice/useReduceradding.jsx
@@ -1,79 +1,135 @@
-import React, { act, Fragment, useReducer, useState } from "react";
+// import React, { act, Fragment, useReducer, useState } from "react";
 
 
-const reducer=(state,action)=>{
-    switch(action.type){
+// const reducer=(state,action)=>{
+//     switch(action.type){
        
-        case "CHANGE_NAME":
-            return {...state,userName:action.payload}
+//         case "CHANGE_NAME":
+//             return {...state,userName:action.payload}
 
-        case "ADD_TODO":
+//         case "ADD_TODO":
 
-            return {...state,todos:[...state.todos,action.payload]}
-    }
+//             return {...state,todos:[...state.todos,action.payload]}
+//     }
 
-}
-const UserReducerExample4=()=>{
+// }
+// const UserReducerExample4=()=>{
 
     
-    const[name,setName]=useState("")
-    const[enteredTodo,setenteredTodo]=useState("")
+//     const[name,setName]=useState("")
+//     const[enteredTodo,setenteredTodo]=useState("")
+//     const initialState={
+//         userName:"ranjith",
+//         age:1,
+//         todos:[]
+//     }
+
+   
+//     const onsubmit=()=>{
+//         dispatch({
+//             type:"CHANGE_NAME",
+//             payload:name
+//         })
+//     }
+
+//     const enterHandler=(event)=>{
+//         setName(event.target.value)
+//     }
+//     const[currentState,dispatch]=useReducer(reducer,initialState)
+
+
+//     const todoHandler=(event)=>{
+//         setenteredTodo(event.target.value)
+//     }
+
+//     const addHandler=()=>{
+//         if(enteredTodo){
+//             dispatch({
+//                 type:"ADD_TODO",
+//                 payload:enteredTodo
+//             })
+            
+//         }
+//         setenteredTodo("")
+//     }
+   
+//     return(
+//         <>
+    
+//         <h1>name:{currentState.userName}</h1>
+//         <input type="text" value={name} onChange={enterHandler}/>
+
+//         <button onClick={onsubmit}>Change Name</button>
+
+
+//         <input type="text" onChange={todoHandler} value={enteredTodo} />
+//         <button onClick={addHandler}>Add Item</button>
+//         {
+//             currentState.todos.map((eachItem)=>{
+//                 return(
+//                     <Fragment>
+//                         <h3>{eachItem}</h3>
+//                     </Fragment>
+//                 )
+//             })
+//         }
+        
+//         </>
+//     )
+// }
+// export default UserReducerExample4
+
+
+import React, { act, useReducer, useState } from "react";
+
+const reducer=(state,action)=>{
+        switch(action.type){
+            case "ADD_ITEM":
+                return {...state,todos:[...state.todos,action.payload]}
+        }
+}
+
+
+const UserReducerExample4=()=>{
     const initialState={
         userName:"ranjith",
-        age:1,
         todos:[]
     }
 
-   
-    const onsubmit=()=>{
-        dispatch({
-            type:"CHANGE_NAME",
-            payload:name
-        })
-    }
 
-    const enterHandler=(event)=>{
+
+
+    const [currentState,dispatch]=useReducer(reducer,initialState)
+    const[name,setName]=useState("")
+
+    const nameHandler=(event)=>{
         setName(event.target.value)
     }
-    const[currentState,dispatch]=useReducer(reducer,initialState)
-
-
-    const todoHandler=(event)=>{
-        setenteredTodo(event.target.value)
-    }
-
     const addHandler=()=>{
-        if(enteredTodo){
+        if(setName){
             dispatch({
-                type:"ADD_TODO",
-                payload:enteredTodo
+                type:"ADD_ITEM",
+                payload:name
             })
-            
         }
-        setenteredTodo("")
+        setName("")
     }
-   
+    
+
     return(
         <>
-    
-        <h1>name:{currentState.userName}</h1>
-        <input type="text" value={name} onChange={enterHandler}/>
-
-        <button onClick={onsubmit}>Change Name</button>
-
-
-        <input type="text" onChange={todoHandler} value={enteredTodo} />
-        <button onClick={addHandler}>Add Item</button>
+        <input type="text" onChange={nameHandler} value={name}/>
+        <button onClick={addHandler}>ADD</button>
         {
             currentState.todos.map((eachItem)=>{
                 return(
-                    <Fragment>
-                        <h3>{eachItem}</h3>
-                    </Fragment>
+                    <>
+                    <h1>{eachItem}</h1>
+                    </>
                 )
             })
         }
-        
+
         </>
     )
 }
