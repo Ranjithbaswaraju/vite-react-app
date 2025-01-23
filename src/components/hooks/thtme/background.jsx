8c0707c5cded09433999a8deb1d86c81a8c52301
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-01-23 20:41:53 +0530

    updated

--- src/components/hooks/thtme/background.jsx
+++ src/components/hooks/thtme/background.jsx
@@ -1,22 +1,19 @@
-import React from "react";
-import { useState } from "react";
+
+import React, { useState } from "react";
 
 const EvenOddBackground=()=>{
   const[number,setNumber]=useState("")
 
-  const colorHandler=(event)=>{
+  const chnageHandler=(event)=>{
     setNumber(event.target.value)
   }
 
-  const backgroundColor=number%2===0 ?'black':'orange'
+  const backgroundColor=number%2==0 ? "green" : "violet"
   return(
-    <>
-    <div style={{backgroundColor,height:'100vh',width:'100vw' ,display:'flex',justifyContent:'center',alignItems:'center'}}>
-    <input type="text" value={number} onChange={colorHandler} placeholder="enter a to change background color"/>
-    <p>{}</p>
+    <div style={{backgroundColor,height:'100vh',width:'100vw', display:'flex', justifyContent:'center',alignItems:"center"}}>
+        <input type="text" value={number} onChange={chnageHandler} placeholder="enter the number"/>
+        
     </div>
-    </>
   )
 }
 export default EvenOddBackground
-
