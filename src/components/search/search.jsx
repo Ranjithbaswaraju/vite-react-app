46ace0433ec1f4b7fcc757f75f2db47e9bc18adc
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-01-24 16:29:30 +0530

    updated

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,87 +1,60 @@
 
 
-// import axios from "axios";
-// import React, { useEffect, useState } from "react";
-
-// const Search=()=>{
-//     const[products,setProducts]=useState([])
-//     const[searchQuery,setSearchQuery]=useState()
-//     const[filteredData,setFilterdData]=useState([])
-// useEffect(()=>{
-//     fecthData()
-
-//     },[])
-
-//     const fecthData=async()=>{
-//         const finalData=await axios.get('https://dummyjson.com/products')
-//         setProducts(finalData.data.products)
-//         setFilterdData(finalData.data.products)
-
-//     }
-//     const clickHandler=(e)=>{
-//         const query=e.target.value.toLowerCase()
-//         setSearchQuery(query)
-//         // console.log(setSearchQuery)
-
-
-//         const filterData=products.filter(products=>
-//             products.title.toLowerCase().includes(query)
-//         )
-//         setFilterdData(filterData)
-//     }
-//     return(
-//         <>
-//         <input type="text" onChange={clickHandler}  value={searchQuery}/>
-//         {
-//             <ol>
-//                 {
-//                     filteredData.length>0?(
-//                         filteredData.map(product=>(
-//                         <>
-//                         <li>{product.title}</li>
-//                         </>
-//                     ))
-//                 ):
-//                 (
-//                     <li style={{color:'red'}}>No produts found</li>
-//                 )
-//                 }
-//             </ol>
-            
-//         }
-//         </>
-//     )
-// }
-
-
-
-
-// export default Search
-
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-
 const Search=()=>{
+    const[products,setProducts]=useState([])
+    const[searchQuery,setSearchQuery]=useState()
+    const[filteredData,setFilterdData]=useState([])
+useEffect(()=>{
+    fecthData()
 
-    const[product,setProducts]=useState([])
-    const[filterData,setFilterdData]=useState([])
-    const[searchQuary,setSearchQuary]=useState()
-
-    useEffect(()=>{
-        fetchData()
     },[])
 
-    const fetchData=async()=>{
-        const response=await axios.get('https://dummyjson.com/recipes')
-        setProducts(response.data)
-       
+    const fecthData=async()=>{
+        const finalData=await axios.get('https://dummyjson.com/products')
+        setProducts(finalData.data.products)
+        setFilterdData(finalData.data.products)
+
     }
-    console.log(setProducts)
+    const clickHandler=(e)=>{
+        const query=e.target.value.toLowerCase()
+        setSearchQuery(query)
+        // console.log(setSearchQuery)
+
+
+        const filterData=products.filter(products=>
+            products.title.toLowerCase().includes(query)
+        )
+        setFilterdData(filterData)
+    }
     return(
         <>
-
+        <input type="text" onChange={clickHandler}  value={searchQuery}/>
+        {
+            <ol>
+                {
+                    filteredData.length>0?(
+                        filteredData.map(product=>(
+                        <>
+                        <li>{product.title}</li>
+                        </>
+                    ))
+                ):
+                (
+                    <li style={{color:'red'}}>No produts found</li>
+                )
+                }
+            </ol>
+            
+        }
         </>
     )
 }
-export default Search;
+
+
+
+
+export default Search
+
