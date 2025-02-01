b8fcdd56e0f7a10281252b57255c0b5d17449253
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-02-01 17:52:17 +0530

    updated

--- src/Practice/useReducer.jsx
+++ src/Practice/useReducer.jsx
@@ -1,36 +1,71 @@
+// import React, { useReducer } from "react";
+// const reducer=(state,action)=>{
+//     switch(action.type){
+//         case "INCREMENT_AGE":
+//             return {...state,age:state.age+1}
+//         default:
+//             return state
+//     }
+// }
+// const UserReducerExample=()=>{
+//     const initialState={
+//         username:"raju",
+//         age:0,
+//         todos:["woke up at 8am", "Breakfast at 10am"],
+//         homeaddress:{}
+//     }
+//     const[currentState,dispatch]=useReducer(reducer,initialState)
+//     const incrementHandler=()=>{
+//         dispatch({
+//             type:"INCREMENT_AGE",
+//             payload:2
+//         })
+//     }
+//     return(
+//         <>
+//         <h1>{currentState.username}</h1>
+//         <h1>{currentState.age}</h1>
+//         <button onClick={incrementHandler}>Increment age</button>
+//         </>
+//     )
+// }
+// export default UserReducerExample
+
+
+
+
+
 import React, { useReducer } from "react";
+
+
 const reducer=(state,action)=>{
     switch(action.type){
-        case "INCREMENT_AGE":
-            return {...state,age:state.age+1}
+        case "INC":
+            return {...state,age:state.age+action.payload}
         default:
             return state
     }
-
 }
 const UserReducerExample=()=>{
+
     const initialState={
-        username:"raju",
-        age:0,
-        todos:["woke up at 8am", "Breakfast at 10am"],
-        homeaddress:{}
+        age:10
     }
     const[currentState,dispatch]=useReducer(reducer,initialState)
-    const incrementHandler=()=>{
+
+    const clickHandler=()=>{
         dispatch({
-            type:"INCREMENT_AGE",
+            type:"INC",
             payload:2
         })
     }
     return(
         <>
-        <h1>{currentState.username}</h1>
-        <h1>{currentState.age}</h1>
-        <button onClick={incrementHandler}>Increment age</button>
+
+        <p>{currentState.age}</p>
+        <button onClick={clickHandler}>Click Here</button>
         </>
     )
 }
+
 export default UserReducerExample
-
-
-
