4a4a91dc0a46c5e4a2d139757ea79513bdc046fe
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-02-01 17:31:58 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,83 @@
+// import React from "react";
+
+
+// const Grand=({message})=>{
+//   return(
+//     <>
+//     <h1>Grand Child Component</h1>
+//     <p>Last message: {message}</p>
+//     </>
+//   )
+// }
+
+// const Child=({message})=>{
+//   return(
+//     <>
+//     <h1>Child Component</h1>
+//     <Grand message={message}/>
+//     </>
+//   )
+// }
+
+
+// const Parent=({message})=>{
+//   return(
+//     <>
+//     <h1>Parent Component</h1>
+//     <Child message={message}/>
+//     </>   
+//   )
+// }
+
+
+// const App=()=>{
+//   const message="Hello World"
+//   return(
+//     <>
+//     <h1>App Component</h1>
+//     <Parent message={message}/>
+//     </>
+//   )
+// }
+// export default App
+
+
 import React from "react";
 
 
-const Grand=({message})=>{
+const Grand=({ranjith})=>{
   return(
     <>
-    <h1>Grand Child Component</h1>
-    <p>Last message: {message}</p>
+    <p>{ranjith}</p>
+    </>
+  )
+}
+const Child=({ranjith})=>{
+  return(
+    <>
+    <h1>GrandChild</h1>
+    <Grand ranjith={ranjith}/>
     </>
   )
 }
 
-const Child=({message})=>{
-  return(
-    <>
-    <h1>Child Component</h1>
-    <Grand message={message}/>
-    </>
-  )
-}
-
-
-const Parent=({message})=>{
+const Parent=({ranjith})=>{
   return(
     <>
     <h1>Parent Component</h1>
-    <Child message={message}/>
-    </>   
-  )
-}
-
-
-const App=()=>{
-  const message="Hello World"
-  return(
-    <>
-    <h1>App Component</h1>
-    <Parent message={message}/>
+    <Child ranjith={ranjith}/>
     </>
   )
 }
-export default App
+const App=()=>{
+  const message='RANJITH'
+  return(
+    <>
+
+
+    <h1>App component</h1>
+    <Parent ranjith={message}/>
+    </>
+  )
+}
+export default App;
