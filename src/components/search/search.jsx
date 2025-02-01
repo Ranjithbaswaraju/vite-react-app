17e7664e77ebe85ccb219a2f3255439d98d65e02
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-02-01 20:53:29 +0530

    updated

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,60 +1,106 @@
+// import axios from "axios";
+// import React from "react";
+// import { useEffect } from "react";
+// import { useState } from "react";
+
+// const Search=()=>{
+//     const[products,setProducts]=useState([])
+//     const[searchQuery,setSearchQuery]=useState("")
+//     const[filterData,setFilterdData]=useState([])
+
+//     useEffect(()=>{
+//         fetchData()
+//     },[])
+
+//     const fetchData=async()=>{
+//             const response=await axios.get('https://dummyjson.com/products')
+//             setProducts(response.data.products)
+//            
+//     }
+//     const handler=(event)=>{
+//         event.preventDefault()
+
+//         const query=event.target.value.toLowerCase()
+//         setSearchQuery(query)
+
+//         const filteredData=products.filter((item)=>{
+//             return item.title.toLowerCase().includes(query)
+//         })
+//         setFilterdData(filteredData)
+//     }
+//     return(
+//         <>
+//         <input type="text" onChange={handler} value={searchQuery}/>
+//             {
+//                 filterData.length>0?
+
+//                 filterData.map((item)=>{
+//                     return(
+//                         <>
+//                             <h1>{item.title}</h1>
+//                         </>
+//                     )
+//                 }):
+//                 (
+//                     <li style={{color:'red'}}>No resultsf ofun</li>
+//                 )
+//             }
+//         </>
+//     )
+// }
+// export default Search;
 
 
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Search=()=>{
-    const[products,setProducts]=useState([])
-    const[searchQuery,setSearchQuery]=useState()
-    const[filteredData,setFilterdData]=useState([])
-useEffect(()=>{
-    fecthData()
 
+    const[products,setProducts]=useState([])
+    const[searchQuery,setSearchQuery]=useState("")
+    const[filterdData,setFilterdData]=useState([])
+    
+    useEffect(()=>{
+        finalData()
     },[])
 
-    const fecthData=async()=>{
-        const finalData=await axios.get('https://dummyjson.com/products')
-        setProducts(finalData.data.products)
-        setFilterdData(finalData.data.products)
-
+    const finalData=async()=>{
+        
+        const response=await axios.get('https://dummyjson.com/recipes')
+        setProducts(response.data.recipes)
+        
     }
-    const clickHandler=(e)=>{
-        const query=e.target.value.toLowerCase()
+
+    const changeHandler=(event)=>{
+
+        const query=event.target.value.toLowerCase()
         setSearchQuery(query)
-        // console.log(setSearchQuery)
 
 
-        const filterData=products.filter(products=>
-            products.title.toLowerCase().includes(query)
-        )
-        setFilterdData(filterData)
+        const filter=products.filter((items)=>{
+            return items.name.toLowerCase().includes(query)
+        })
+        setFilterdData(filter)
     }
+
     return(
         <>
-        <input type="text" onChange={clickHandler}  value={searchQuery}/>
+        <input type="text" onChange={changeHandler} value={searchQuery}/>
         {
-            <ol>
-                {
-                    filteredData.length>0?(
-                        filteredData.map(product=>(
-                        <>
-                        <li>{product.title}</li>
-                        </>
-                    ))
-                ):
-                (
-                    <li style={{color:'red'}}>No produts found</li>
+            filterdData.length>0?
+            filterdData.map((item)=>{
+                return(
+                    <>
+                    <li>{item.name}</li>
+                    </>
                 )
-                }
-            </ol>
-            
+            }):(
+                <li style={{color:'red'}}>No result Found</li>
+            )
         }
         </>
     )
 }
-
-
-
-
 export default Search
-
