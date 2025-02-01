e24b68260d10d5485067d35a5247e613f750254b
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-02-01 19:06:09 +0530

    updated

--- src/Practice/crud.jsx
+++ src/Practice/crud.jsx
@@ -1,55 +1,88 @@
 
 
+// import React, { useState } from "react";
+
+// const CrudOperations=()=>{
+
+//     const[todos,setTodos]=useState([])
+//     const[input,setInput]=useState("")
+
+//     const InputHandler=(event)=>{
+//         setInput(event.target.value)
+
+//     }
+//     const addHandler=(event)=>{
+//         event.preventDefault()
+//         if(input){
+//             setTodos([...todos,input])
+//         }
+//         setInput("")
+       
+//     }
+
+//     const deleteHandler=(ind)=>{
+//         const updatedTodo=   todos.filter((_,index)=>index!=ind)
+//         setTodos(updatedTodo)   
+//     }
+//     return(
+//         <>
+//         <form onSubmit={addHandler}>
+//             <input value={input} onChange={InputHandler}/>
+//             <button type="submit">ADD TODO</button>
+           
+//         </form>
+//         {
+//                 todos?.map((eachItem,index)=>{
+//                     return(
+//                         <>
+//                         <h3>{eachItem}</h3>
+//                         <button onClick={()=>deleteHandler(index)}>DELETE</button>
+//                         <button>UPDATE</button>
+//                         </>
+//                     )
+//                 })
+//             }
+        
+//         </>
+//     )
+// }
+// export default CrudOperations
+
+
 import React, { useState } from "react";
 
 const CrudOperations=()=>{
-
-    const[todos,setTodos]=useState([])
     const[input,setInput]=useState("")
-
-    const InputHandler=(event)=>{
-        setInput(event.target.value)
-
-    }
+    const[todos,setTodos]=useState([])
 
     const addHandler=(event)=>{
-        event.preventDefault()
+        setInput(event.target.value)
+    }
+    const add1Handler=()=>{
         if(input){
             setTodos([...todos,input])
         }
         setInput("")
-       
     }
-
     const deleteHandler=(ind)=>{
-        const updatedTodo=   todos.filter((_,index)=>index!=ind)
+        const updatedTodo=todos.filter((_,index)=>index!=ind)
         setTodos(updatedTodo)
-        
     }
-    
-
-
-
-
     return(
         <>
-        <form onSubmit={addHandler}>
-            <input value={input} onChange={InputHandler}/>
-            <button type="submit">ADD TODO</button>
-           
-        </form>
-        {
-                todos?.map((eachItem,index)=>{
+            <input type="text" value={input} onChange={addHandler}/>
+            <button onClick={add1Handler}>ADD</button>
+
+            {
+                todos?.map((item,index)=>{
                     return(
                         <>
-                        <h3>{eachItem}</h3>
+                        <li>{item}</li>
                         <button onClick={()=>deleteHandler(index)}>DELETE</button>
-                        <button>UPDATE</button>
                         </>
                     )
                 })
             }
-        
         </>
     )
 }
