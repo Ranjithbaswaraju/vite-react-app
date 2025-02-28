5c6be69fd5d586fa104ba1c263fbcf6b24183d99
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-02-28 22:08:28 +0530

    updated

--- src/Practice/useMemoSearch.jsx
+++ src/Practice/useMemoSearch.jsx
@@ -1,35 +1,37 @@
 
 
-import React, { useMemo, useState } from "react";
-const UseMemoSearch=()=>{
-    const[search,setSearch]=useState("")
-    const items=['apple','bob','cat']
-    const changeHandler=(event)=>{
-        setSearch(event.target.value)
-    }
-    const filteredData=useMemo(()=>{
-        return items.filter((item)=>(
-            item.toLowerCase().includes(search.toLowerCase())
-        ))
-    },[search])
-    return(
-        <>
-        <input type="text" value={search} onChange={changeHandler}/>
-        {
-            filteredData.length===0?(<p style={{color:'red'}}>No result found</p>)
-            :(
-                <ol >
-               {
-                 filteredData.map((ranjith,index)=>(
+// import React, { useMemo, useState } from "react";
+// const UseMemoSearch=()=>{
+//     const[search,setSearch]=useState("")
+//     const items=['apple','bob','cat']
+//     const changeHandler=(event)=>{
+//         setSearch(event.target.value)
+//     }
+//     const filteredData=useMemo(()=>{
+//         return items.filter((item)=>(
+//             item.toLowerCase().includes(search.toLowerCase())
+//         ))
+//     },[search])
+//     return(
+//         <>
+//         <input type="text" value={search} onChange={changeHandler}/>
+//         {
+//             filteredData.length===0?(<p style={{color:'red'}}>No result found</p>)
+//             :(
+//                 <ol >
+//                {
+//                  filteredData.map((ranjith,index)=>(
                     
-                    <li key={index}>{ranjith}</li>
-                ))}
-                </ol>
+//                     <li key={index}>{ranjith}</li>
+//                 ))}
+//                 </ol>
                 
-            )
-        }
+//             )
+//         }
       
-        </>
-    )
-}
-export default UseMemoSearch
+//         </>
+//     )
+// }
+// export default UseMemoSearch
+
+
