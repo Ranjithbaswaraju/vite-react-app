1c6e3e57424565c62360b4f3eac6ee3c7ba41d01
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-02-28 15:05:20 +0530

    updated

--- src/Practice/uncontrolled.jsx
+++ src/Practice/uncontrolled.jsx
@@ -1,24 +1,45 @@
-import React from "react";
-import { useRef } from "react";
+// import React from "react";
+// import { useRef } from "react";
+// const Uncontrolled=()=>{
+//     const inputRef=useRef("")
+//     const clickHandler=(event)=>{
+//         event.preventDefault()
+//         alert(`Entered Name is ${inputRef.current.value}`)
+//     }
+//     return(
+//         <>
+//         <form onSubmit={clickHandler}>
+//             <label>
+//                 Name:
+//                 <input type="text" placeholder="enter your name" ref={inputRef}/>
+
+//             </label>
+//             <button type="submit">Click</button>
+//         </form>
+//         </>
+//     )
+// }
+// export default Uncontrolled
+
+
+import React, { useRef } from "react";
+
 const Uncontrolled=()=>{
-    const inputRef=useRef("")
-    const clickHandler=(event)=>{
+
+    const inputRef=useRef('')
+
+    const submit=(event)=>{
         event.preventDefault()
-        alert(`Entered Name is ${inputRef.current.value}`)
+        alert(`Entered name is : ${inputRef.current.value}`)
     }
     return(
         <>
-        <form onSubmit={clickHandler}>
-            <label>
-                Name:
-                <input type="text" placeholder="enter your name" ref={inputRef}/>
-
-            </label>
-            <button type="submit">Click</button>
+        <form onSubmit={submit}>
+        <input type='text' ref={inputRef}/>
+        <button type="submit">Submit</button>
         </form>
+        
         </>
     )
 }
 export default Uncontrolled
-
-
