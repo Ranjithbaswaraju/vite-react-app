41ab797ee24080223034f5ea1f9c56cad239a717
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-02-28 21:55:48 +0530

    updated

--- src/components/hooks/thtme/background.jsx
+++ src/components/hooks/thtme/background.jsx
@@ -1,18 +1,39 @@
 
+// import React, { useState } from "react";
+
+// const EvenOddBackground=()=>{
+//   const[number,setNumber]=useState("")
+
+//   const chnageHandler=(event)=>{
+//     setNumber(event.target.value)
+//   }
+
+//   const backgroundColor=number%2==0 ? "green" : "violet"
+//   return(
+//     <div style={{backgroundColor,height:'100vh',width:'100vw', display:'flex', justifyContent:'center',alignItems:"center"}}>
+//         <input type="text" value={number} onChange={chnageHandler} placeholder="enter the number"/>
+        
+//     </div>
+//   )
+// }
+// export default EvenOddBackground
+
+
 import React, { useState } from "react";
 
-const EvenOddBackground=()=>{
+
+const EvenOddBackground = () => {
   const[number,setNumber]=useState("")
 
-  const chnageHandler=(event)=>{
-    setNumber(event.target.value)
+  const evenHandler=(e)=>{
+    setNumber(e.target.value)
   }
 
-  const backgroundColor=number%2==0 ? "green" : "violet"
+const backgroundColor=number%2==0?"green":"violet"
+
   return(
-    <div style={{backgroundColor,height:'100vh',width:'100vw', display:'flex', justifyContent:'center',alignItems:"center"}}>
-        <input type="text" value={number} onChange={chnageHandler} placeholder="enter the number"/>
-        
+    <div style={{backgroundColor,height:"100vh",width:"100%",display:'flex',justifyContent:"center",alignItems:"center"}}>
+      <input type="text" value={number} onChange={evenHandler}/>
     </div>
   )
 }
--- src/components/hooks/thtme/evenorodd.jsx
+++ src/components/hooks/thtme/evenorodd.jsx
@@ -1,21 +1,42 @@
 
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
+
+// const Even=()=>{
+//     const[number,setNumber]=useState("")
+
+//     const handler=(event)=>{
+//         setNumber(event.target.value)
+//     }
+//     const result=isNaN(number)||number==""?"":
+//     number%2===0?'even ':"Odd"
+
+//     return(
+//         <>
+//         <input value={number} onChange={handler}/>
+//         <p>The entered vale is {result}</p>
+//         </>
+//     )
+// }
+// export default Even
+
+import React, { useState } from "react";
 
 const Even=()=>{
     const[number,setNumber]=useState("")
 
     const handler=(event)=>{
-        setNumber(event.target.value)
+        setNumber(event.target.value)   
     }
-    const result=isNaN(number)||number==""?"":
-    number%2===0?'even ':"Odd"
+
+    const result=isNaN(number)||number===""?"":
+    number%2==0?"even":"odd"
 
     return(
-        <>
-        <input value={number} onChange={handler}/>
-        <p>The entered vale is {result}</p>
-        </>
+            <>
+            <input type="text" value={number} onChange={handler}/>
+            <p>The numebr is :{result}</p>
+            </>
     )
 }
 export default Even
