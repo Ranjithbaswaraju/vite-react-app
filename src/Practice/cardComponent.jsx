eeb57f3dc587293ba8972c8db369a8bd0795a5c1
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-02-28 16:36:05 +0530

    updated

--- src/Practice/cardComponent.jsx
+++ src/Practice/cardComponent.jsx
@@ -1,18 +1,40 @@
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+
+// function CardComponent({title,image,description,price,ranjith}) {
+//   return (
+//     <Card style={{ width: '300px',  height:"450px", display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent:'center',textAlign:'center'}}>
+//         <Card.Title style={{height:"20px", display:'-webkit-box',WebkitBoxOrient:"vertical",overflow:'hidden',WebkitLineClamp:1}}>{title}</Card.Title>
+//       <Card.Img  src={image} style={{height:"200px", width:"250px", }}/>
+//       <Card.Body >
+        
+//         <Card.Text style={{display: "-webkit-box", WebkitBoxOrient: "vertical", overflow: "hidden", WebkitLineClamp: 4 }}>
+//         {description}
+//         </Card.Text>
+//         <Card.Title >{ranjith}</Card.Title>
+//         <Button >{price}</Button>
+//       </Card.Body>
+//     </Card>
+//   );
+// }
+
+// export default CardComponent;
+
+
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function CardComponent({title,image,description,price,ranjith}) {
+function CardComponent({title,category,description,image}) {
   return (
-    <Card style={{ width: '300px',  height:"450px", display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent:'center',textAlign:'center'}}>
-        <Card.Title style={{height:"20px", display:'-webkit-box',WebkitBoxOrient:"vertical",overflow:'hidden',WebkitLineClamp:1}}>{title}</Card.Title>
-      <Card.Img  src={image} style={{height:"200px", width:"250px", }}/>
-      <Card.Body >
-        
-        <Card.Text style={{display: "-webkit-box", WebkitBoxOrient: "vertical", overflow: "hidden", WebkitLineClamp: 4 }}>
-        {description}
+    <Card style={{ width: '400px',height:'500px', display:'flex',justifyContent:'center',alignItems:'center' }}>
+      <Card.Img src={image}  style={{height:'200px',width:'200px'}}/>
+      <Card.Body>
+        <Card.Title style={{display:'-webkit-box',WebkitBoxOrient:"vertical",overflow:'hidden',WebkitLineClamp:2}}>{title}</Card.Title>
+        <Card.Text>{category}</Card.Text>
+        <Card.Text style={{display:'-webkit-box',WebkitBoxOrient:"vertical",overflow:'hidden',WebkitLineClamp:3}}>
+          {description}
         </Card.Text>
-        <Card.Title >{ranjith}</Card.Title>
-        <Button >{price}</Button>
+        <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
   );
--- src/Practice/useEffectfetch.jsx
+++ src/Practice/useEffectfetch.jsx
@@ -1,45 +1,88 @@
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
+// import CardComponent from "./cardComponent";
+
+// const UseEffectfetch=()=>{
+//     const[data,setData]=useState([])
+
+
+//     useEffect(()=>{
+//         const data1=async()=>{
+//             try{
+//                 const response=await axios.get('https://fakestoreapi.com/products')
+//                 setData(response.data)
+//             }
+//             catch(error){
+//                 console.log("The error is ",error)
+//             }
+            
+//         }
+//         data1()
+
+//     },[])
+//     return(
+//         <div style={{display:'flex',flexWrap:"wrap", gap:"50px",justifyContent:'center'}}>
+
+//     {
+//         data.map((item)=>{
+//             return(
+//                 <>
+//                 <CardComponent  
+//                 key={item.id}
+//                 title={item.title}
+//                 image={item.image}
+//                 description={item.description}
+//                 price={item.price}
+//                 ranjith={item.category}
+//                 />
+//                 </>
+//             )
+//         })
+//     }
+        
+//         </div>
+//     )
+// }
+// export default UseEffectfetch
+
+
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import CardComponent from "./cardComponent";
 
-const UseEffectfetch=()=>{
+const UseEffectfetch = () => {
     const[data,setData]=useState([])
-
 
     useEffect(()=>{
         const data1=async()=>{
             try{
                 const response=await axios.get('https://fakestoreapi.com/products')
                 setData(response.data)
+                console.log(response.data)
             }
-            catch(error){
-                console.log("The error is ",error)
+            catch(err){
+                console.log(err)
             }
-            
         }
         data1()
+    })
 
-    },[])
     return(
-        <div style={{display:'flex',flexWrap:"wrap", gap:"50px",justifyContent:'center'}}>
-
-    {
-        data.map((item)=>{
-            return(
-                <>
-                <CardComponent  
-                key={item.id}
-                title={item.title}
-                image={item.image}
-                description={item.description}
-                price={item.price}
-                ranjith={item.category}
-                />
-                </>
-            )
-        })
-    }
-        
+        <div style={{height:"100%",width:'100%',display:'flex',flexWrap:'wrap',gap:"10px",justifyContent:'center'
+        }}>
+            {
+                data.map((each)=>{
+                    return(
+                        <>
+                        <CardComponent
+                        title={each.title}
+                        category={each.category}
+                        description={each.description}
+                        image={each.image}/>
+                        </>
+                    )
+                })
+            }
         </div>
     )
 }
