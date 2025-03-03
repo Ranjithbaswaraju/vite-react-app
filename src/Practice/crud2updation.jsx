82a425f219d831e4df79b17c0f9ed9b1f9969aed
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-03-03 09:00:50 +0530

    uPdated

--- src/Practice/crud2updation.jsx
+++ src/Practice/crud2updation.jsx
@@ -1,61 +1,107 @@
+// import React, { useState } from "react";
+
+// const CrudUpdation=()=>{
+
+//     const[input,setInput]=useState("")
+//     const[todos,setTodos]=useState([])
+//     const[selectedItem,setSelectedItem]=useState(null)
+
+//     const inputHandler=(event)=>{
+//         setInput(event.target.value)
+//     }
+//     const ADD_TODO=(event)=>{
+//         event.preventDefault()
+//         if(input){
+//             setTodos([...todos,input])
+//         }
+//         setInput("")
+//     }
+//     const deleteHandler=(ind)=>{
+//         const updatedTodo=todos.filter((_,index)=>index!=ind)
+//         setTodos(updatedTodo)
+//     }
+//     const updateHandler=(ind)=>{
+//         setSelectedItem(ind)
+//         const inputValue=todos[ind]
+//         setInput(inputValue)
+//     }
+//     const updateTodo=(event)=>{
+//         event.preventDefault()
+//         const todoCopy=[...todos]
+//         todoCopy[selectedItem]=input
+//         setTodos(todoCopy)
+//         setSelectedItem("")
+//         setInput("")
+
+//     }
+//     return(
+//         <>
+//         <form onSubmit={selectedItem || selectedItem===0 ? updateTodo : ADD_TODO}>
+//         <input type="text" value={input} onChange={inputHandler}/>
+//         <button type="submit">{selectedItem || selectedItem===0 ? "updateTodo" : "ADD_TODO"}</button>
+//         </form>
+//         {
+//             todos?.map((item,index)=>{
+//                 return(
+//                     <>
+//                     <li>{item}</li>
+//                     <button onClick={()=>deleteHandler(index)}>DELETE_TODO</button>
+//                     <button onClick={()=>updateHandler(index)}>UPDATE</button>
+//                     </>
+//                 )
+//             })
+//         }
+//         </>
+//     )
+// }
+// export default CrudUpdation
+
+
 import React, { useState } from "react";
 
-const CrudUpdation=()=>{
-
-    const[input,setInput]=useState("")
+const CrudUpdation = () => {
+    const[name,setName]=useState("")
     const[todos,setTodos]=useState([])
-    const[selectedItem,setSelectedItem]=useState(null)
+    const[selectItem,setSelectItem]=useState(null)
 
-    const inputHandler=(event)=>{
-        setInput(event.target.value)
+    const changeHandler=(event)=>{
+        setName(event.target.value)
     }
-    const ADD_TODO=(event)=>{
+
+    const addHandler=(event)=>{
         event.preventDefault()
-        if(input){
-            setTodos([...todos,input])
-        }
-        setInput("")
+        setTodos([...todos,name])
+        setName("")
     }
     const deleteHandler=(ind)=>{
-        const updatedTodo=todos.filter((_,index)=>index!=ind)
-        setTodos(updatedTodo)
+        const updated=todos.filter((_,index)=>index!==ind)
+        setTodos(updated)
     }
-    const updateHandler=(ind)=>{
-        setSelectedItem(ind)
-        const inputValue=todos[ind]
-        setInput(inputValue)
-    }
-    const updateTodo=(event)=>{
-        event.preventDefault()
-
-        const todoCopy=[...todos]
-
-        todoCopy[selectedItem]=input
-        setTodos(todoCopy)
-        setSelectedItem("")
-        setInput("")
+    const updatedHandler=(ind)=>{
 
     }
     return(
         <>
+        <form onSubmit={addHandler}>
+            <input type="text" value={name} onChange={changeHandler}/>
+            <button type="submit" >Add_Todo</button>
 
-        <form onSubmit={selectedItem || selectedItem===0 ? updateTodo : ADD_TODO}>
-        <input type="text" value={input} onChange={inputHandler}/>
-        <button type="submit">{selectedItem || selectedItem===0 ? "updateTodo" : "ADD_TODO"}</button>
         </form>
 
+        <ol>
+            {
+                todos?.map((item,index)=>{
+                    return(
+                        <>
+                        <li key={index}>{item}</li>
+                        <button onClick={()=>deleteHandler(index)}>Delete</button>
+                        <button onClick={()=>updatedHandler(index)}>Update</button>
+                        </>
+                    )
+                })
+            }
+        </ol>
 
-        {
-            todos?.map((item,index)=>{
-                return(
-                    <>
-                    <li>{item}</li>
-                    <button onClick={()=>deleteHandler(index)}>DELETE_TODO</button>
-                    <button onClick={()=>updateHandler(index)}>UPDATE</button>
-                    </>
-                )
-            })
-        }
         </>
     )
 }
