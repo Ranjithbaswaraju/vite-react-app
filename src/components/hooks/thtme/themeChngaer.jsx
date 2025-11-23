5cf705c0494226382841c9ae07f10342880c7249
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-11-23 22:36:13 +0530

    updated

--- src/components/hooks/thtme/themeChngaer.jsx
+++ src/components/hooks/thtme/themeChngaer.jsx
@@ -1,26 +1,53 @@
+// import React, { useCallback, useState } from "react";
+
+// const Ranjith=()=>{
+
+//   const[light,setLight]=useState(true)
+
+//   const backgroundHandler=useCallback(()=>{
+//     setLight((light)=>(!light))
+//   })
+
+//   const theme=light?
+//   {
+//     background:"white" , color:"black"
+//   }:
+//   {
+//     background:"black" ,color:"white"
+//   }
+
+//   return(
+//     <div style={{background:theme.background,color:theme.color,height:'100vh',width:'100vw'}}>
+//       <p>The background colo is {light?"white":'dark'}</p>
+//       <button onClick={backgroundHandler}>Click Here</button>
+//     </div>
+//   )
+// }
+// export default Ranjith
+
 import React, { useCallback, useState } from "react";
 
 const Ranjith=()=>{
+  const[light,isLight]=useState(true)
 
-  const[light,setLight]=useState(true)
-
-  const backgroundHandler=useCallback(()=>{
-    setLight((light)=>(!light))
+  const handler=useCallback(()=>{
+    isLight((light)=>(!light))
   })
 
-  const theme=light?
+  const theme=light ?
   {
-    background:"white" , color:"black"
+    background:'white' ,color:'black'
   }:
   {
-    background:"black" ,color:"white"
+    background:'black'  ,color:'white'
   }
+
 
   return(
     <div style={{background:theme.background,color:theme.color,height:'100vh',width:'100vw'}}>
-      <p>The background colo is {light?"white":'dark'}</p>
-      <button onClick={backgroundHandler}>Click Here</button>
+      <p>The background color : {light?"White":"Dark"}</p>
+      <button onClick={handler}>Click Here</button>
     </div>
   )
 }
-export default Ranjith
+export default Ranjith;
