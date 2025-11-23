0ede6a6951b7859304822cd511e49bd89521df1b
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-11-23 11:17:51 +0530

    propdirrling

--- src/App.jsx
+++ src/App.jsx
@@ -41,37 +41,80 @@
 // }
 // export default App
 
-import React  from "react";
+// import React  from "react";
 
-const Grand=({message})=>{
-  return(
-    <h1>{message}</h1>
-  )
-}
+// const Grand=({message})=>{
+//   return(
+//     <h1>{message}</h1>
+//   )
+// }
 
-const Child=({message})=>{
+// const Child=({message})=>{
+//   return(
+//     <>
+//     <h1>Child Component</h1>
+//     <Grand message={message}/>
+//     </>
+//   )
+// }
+// const Parent=({message})=>{
+//   return(
+//     <>
+//     <h1>Parent Component</h1>
+//     <Child message={message}/>
+//     </>
+//   )
+// }
+// const App=()=>{
+//   const message='Hello World'
+//   return(
+//    <>
+//     <h1>App Component</h1>
+//     <Parent message={message}/>
+//    </>
+//   )
+// }
+// export default App
+
+import React from "react";
+
+
+const GrandChildComponent=({ranjith})=>{
   return(
     <>
-    <h1>Child Component</h1>
-    <Grand message={message}/>
+    <p>{ranjith}</p>
     </>
   )
 }
-const Parent=({message})=>{
+
+const ChildComponent=({ranjith})=>{
   return(
     <>
-    <h1>Parent Component</h1>
-    <Child message={message}/>
+    <h1>GrandChildComponent</h1>
+  <GrandChildComponent ranjith={ranjith}/>
+    </>
+  )
+
+}
+
+
+const ParentComponent=({ranjith})=>{
+  return(
+    <>
+    <h1>ChildComponent</h1>
+  <ChildComponent ranjith={ranjith}/>
     </>
   )
 }
+
+
 const App=()=>{
   const message='Hello World'
   return(
-   <>
+    <>
     <h1>App Component</h1>
-    <Parent message={message}/>
-   </>
+    <ParentComponent ranjith={message}/>
+    </>
   )
 }
-export default App
+export default App;
