4236faf6fe674c7d04b42166d2e20b3283a94cf4
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-11-24 20:58:12 +0530

    updated

--- src/components/hooks/thtme/background.jsx
+++ src/components/hooks/thtme/background.jsx
@@ -19,25 +19,49 @@
 // export default EvenOddBackground
 
 
+// import React, { useState } from "react";
+
+
+// const EvenOddBackground=()=>{
+//   const[number,setNumber]=useState("")
+//   const handler=(e)=>{
+//     setNumber(e.target.value)
+//   }
+
+//   let backgroundColor='white'
+//   if(number !== "" && !isNaN(number)){
+//     backgroundColor = number%2===0 ? 'black' :'violet'
+//   }
+
+//   return(
+//     <div style={{backgroundColor,height:'100vh',width:'100vw',display:'flex',justifyContent:'center',alignItems:'center'}}>
+//       <input type='text' value={number} onChange={handler}/>
+//     </div>
+//   )
+
+// }
+// export default EvenOddBackground;
+
+
 import React, { useState } from "react";
 
 
 const EvenOddBackground=()=>{
   const[number,setNumber]=useState("")
 
-
-  const handler=(e)=>{
+const handler=(e)=>{
     setNumber(e.target.value)
-  }
+}
 
-  let backgroundColor='white'
-  if(number !== "" && !isNaN(number)){
-    backgroundColor = number%2===0 ? 'black' :'violet'
-  }
+let backgroundColor='white'
+
+if(number!==" " && !isNaN(number)){
+    backgroundColor= number%2===0 ? 'black' :'violet'
+}
 
   return(
     <div style={{backgroundColor,height:'100vh',width:'100vw',display:'flex',justifyContent:'center',alignItems:'center'}}>
-      <input type='text' value={number} onChange={handler}/>
+      <input type="text" value={number} onChange={handler}/>
     </div>
   )
 
