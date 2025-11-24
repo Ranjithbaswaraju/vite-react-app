814e99c216fc9ed26a8ecc1848b9f2772ade047b
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-11-24 19:35:39 +0530

    updated

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -49,53 +49,83 @@
 // }
 // export default Search;
 
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
 
-const Search=()=>{
-    const[data,setData]=useState([])
-    const[input,setInput]=useState("")
+// const Search=()=>{
+//     const[data,setData]=useState([])
+//     const[input,setInput]=useState("")
 
-useEffect(()=>{
-    const fetchData=async()=>{
-        try{
-            const response=await axios.get('https://dummyjson.com/products')
-            setData(response.data.products)
-        }
-        catch(err){
-            console.log(err)
-        }
-    }
-    fetchData()
-},[input])
+// useEffect(()=>{
+//     const fetchData=async()=>{
+//         try{
+//             const response=await axios.get('https://dummyjson.com/products')
+//             setData(response.data.products)
+//         }
+//         catch(err){
+//             console.log(err)
+//         }
+//     }
+//     fetchData()
+// },[input])
     
 
-    const filteredData=data.filter((item)=>{
-        return item.title.toLowerCase().includes(input.toLowerCase())
-    })
+//     const filteredData=data.filter((item)=>{
+//         return item.title.toLowerCase().includes(input.toLowerCase())
+//     })
+
+//     const handler=(e)=>{
+//         setInput(e.target.value)
+//     }
+//     return(
+//         <>
+//         <input onChange={handler} value={input}/>
+//         {
+//             filteredData.length > 0 ?(
+//                 filteredData.map((item)=>{
+//                     return(
+//                         <>
+//                         <p>{item.title}</p>
+//                         </>
+//                     )
+//                 })
+//             ):(
+//                 <p>No data found</p>
+//             )
+//         }
+//         </>
+//     )
+
+    
+// }
+// export default Search;
+
+import React, { useState } from "react";
+
+const Search=()=>{
+    const items=['apple','banana','cat']
+    const[input,setInput]=useState("")
 
     const handler=(e)=>{
         setInput(e.target.value)
     }
+
+    const filterData=items.filter((item)=>{
+        return item.toLowerCase().includes(input.toLowerCase())
+    })
     return(
         <>
-        <input onChange={handler} value={input}/>
+        <input type="text" onChange={handler}/>
         {
-            filteredData.length > 0 ?(
-                filteredData.map((item)=>{
-                    return(
-                        <>
-                        <p>{item.title}</p>
-                        </>
-                    )
-                })
-            ):(
-                <p>No data found</p>
-            )
+            filterData.map((item)=>{
+                return(
+                    <>
+                    <p>{item}</p>
+                    </>
+                )
+            })
         }
         </>
     )
-
-    
 }
 export default Search;
