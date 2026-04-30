8e68de94a55ba4f26e33333fe65afd5f0981ab3b
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2026-04-30 17:24:21 +0530

    m

--- src/Practice/stopwatch.jsx
+++ src/Practice/stopwatch.jsx
@@ -42,47 +42,93 @@
 // export default Stopwatch;
 
 
+// import React, { useState } from "react";
+
+
+// const Stopwatch=()=>{
+//     const[time,setTime]=useState(0);
+//     const[id,setId]=useState(null);
+//     const [isRunning, setIsRunning] = useState(false);
+
+//     const startHandler=()=>{
+//         const interval=setInterval(()=>{
+//             setTime((prev=>prev+1))
+//         },1000)
+//         setId(interval)
+//         setIsRunning(true);
+//     }
+
+//     const stopHandler=()=>{
+//         clearInterval(id)
+//         setIsRunning(false);
+//     }
+
+//     const resetHandler=()=>{
+//         setTime(0)
+//         clearInterval(id)
+//           setIsRunning(false);
+//     }
+
+//     const hours=Math.floor(time/3600).toString().padStart(2,"0")
+//     const minutes=Math.floor((time%3600)/60).toString().padStart(2,"0")
+//     const seconds=Math.floor(time%60).toString().padStart(2,"0")
+
+//     return(
+//         <>
+        
+//         <h1>Stop Watch</h1>
+
+//         {hours}:{minutes}:{seconds}
+
+//         <button onClick={startHandler}>Start</button>
+//         <button onClick={stopHandler}>Stop</button>
+//         <button onClick={resetHandler}>Reset</button>
+//         </>
+//     )
+// }
+// export default Stopwatch;
+
+
+
 import React, { useState } from "react";
 
-
 const Stopwatch=()=>{
-    const[time,setTime]=useState(0);
-    const[id,setId]=useState(null);
-    const [isRunning, setIsRunning] = useState(false);
+const[time,setTime]=useState(0)
+const[id,setId]=useState(null)
+const[running,setIsRunning]=useState(true)
 
-    const startHandler=()=>{
-        const interval=setInterval(()=>{
-            setTime((prev=>prev+1))
-        },1000)
-        setId(interval)
-        setIsRunning(true);
-    }
 
-    const stopHandler=()=>{
-        clearInterval(id)
-        setIsRunning(false);
-    }
+const startHandler=()=>{
+    const interval=setInterval(()=>{
+        setTime((prev=>prev+1))
+    },1000)
+    setId(interval)
+    setIsRunning(true)
+}
 
-    const resetHandler=()=>{
-        setTime(0)
-        clearInterval(id)
-          setIsRunning(false);
-    }
+const stopHandler=()=>{
+    clearInterval(id)
+    setIsRunning(false)
+}
+const resetHandler=()=>{
+    setTime(0)
+    setIsRunning(false)
+}
 
-    const hours=Math.floor(time/3600).toString().padStart(2,"0")
-    const minutes=Math.floor((time%3600)/60).toString().padStart(2,"0")
-    const seconds=Math.floor(time%60).toString().padStart(2,"0")
+const hours=Math.floor(time/3600).toString().padStart(2,"0")
+const minutes=Math.floor((time%3600)/60).toString().padStart(2,"0")
+const seconds=Math.floor(time%60).toString().padStart(2,"0")
 
-    return(
-        <>
-        <h1>Stop Watch</h1>
 
+return(
+    <div>
+    <div>
         {hours}:{minutes}:{seconds}
-
         <button onClick={startHandler}>Start</button>
         <button onClick={stopHandler}>Stop</button>
         <button onClick={resetHandler}>Reset</button>
-        </>
-    )
+    </div>
+    </div>
+)
 }
-export default Stopwatch;
+export default Stopwatch
